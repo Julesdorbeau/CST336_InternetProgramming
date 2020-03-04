1333b57043fd03d7eeb797d69828edcba703865a
Julesdorbeau <jdorbeau@csumb.edu>
2020-03-04 02:15:34 +0100

    Assignment03

--- public/Assignment03/js/javascript.js
+++ public/Assignment03/js/javascript.js
@@ -1,8 +1,7 @@
 document.querySelector('#search').addEventListener('click', research);
-
-function test() {
-    alert($('input').val());
-}
+document.querySelector('#randomSearch').addEventListener('click', randomASearch);
+var randomArray = ['otter', 'baseball','penguin', 'a', 'boat', 'paris', 'london', 'amsterdam', 'chicken', 'b', 'c', 'd','e'];
+$(document).ready(firstDisplay());
 
 function research() {
     $.ajax({
@@ -16,8 +15,45 @@ function research() {
         success: function (result, status) {
             $('#images').html('');
             result.results.forEach(function(r) {
-                $('#images').append('<div class="col-sm-4"><br><img src="'+r.urls.small+'" alt="Error"></div>');
+                $('#images').append('<div class="col-sm-4"><br><p class="img-desc"> Description : <p class="img-desc-suit">'+ r.alt_description +'</p></p><img src="'+r.urls.small+'" alt="Error"></div>');
             });
         }
     });
+}
+
+function firstDisplay(){
+    for(var i = 0; i<3; i++) {
+        $.ajax({
+            method: "GET",
+            url: "https://api.unsplash.com/search/photos",
+            dataType: "json",
+            data: {
+                query : 'random',
+                client_id : 'vBqGLd_U7WuAvwbjXxAqfpWmMtDCADqHPqUaMDwr2po',
+            },
+            success: function (result, status) {
+                var toDisplay = Math.floor(Math.random() * result.results.length);
+                $('#images').append('<div class="col-sm-4" id="start"><br><p class="img-desc-start"> Description : <p class="img-desc-suit">'+ result.results[toDisplay].alt_description +'</p></p><img src="'+result.results[toDisplay].urls.small+'" alt="Error"></div>');
+            }
+        });
+    }
+}
+
+function randomASearch() {
+    $('#images').html('');
+    for(var i = 0; i<6; i++) {
+        $.ajax({
+            method: "GET",
+            url: "https://api.unsplash.com/search/photos",
+            dataType: "json",
+            data: {
+                query: randomArray[Math.floor(Math.random() * randomArray.length)],
+                client_id: 'vBqGLd_U7WuAvwbjXxAqfpWmMtDCADqHPqUaMDwr2po',
+            },
+            success: function (result, status) {
+                var toDisplay = Math.floor(Math.random() * result.results.length);
+                $('#images').append('<div class="col-sm-4" id="random"><br><p class="img-desc-start"> Description : <p class="img-desc-suit">' + result.results[toDisplay].alt_description + '</p></p><img src="' + result.results[toDisplay].urls.small + '" alt="Error"></div>');
+            }
+        });
+    }
 }
